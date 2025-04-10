ff0ad95335eb1017f35c0bab5a0048f507f1718b
David Allen <dallen@merceradvisors.com>
2025-04-10 06:54:49 -0700

    Add strategy_return function

---
+++ R/strategy_return.R
@@ -0,0 +1,44 @@
+#' Calculate the return (net or gross) of a MA strategy
+#'
+#' @param strategy_name the name of an MA strategy
+#' @param start yyyy-mm-dd
+#' @param end yyyy-mm-dd
+#' @param type either "net" or "gross"
+#'
+#' @returns a numeric
+#' @export
+#'
+strategy_return <- function(
+    strategy_name,
+    start,
+    end,
+    type = "net") {
+
+  Strategy <- Date <- Value <- NULL
+
+  # Read either gross or net indexes dataset.
+  if(type == "net"){
+    index <- readr::read_csv(
+      file = "~/Data/strategy_indexes_net.csv",
+      show_col_types = FALSE)
+  } else {
+    if(type == "gross"){
+      index <- readr::read_csv(
+        file = "~/Data/strategy_indexes_gross.csv",
+        show_col_types = FALSE)
+    }
+  }
+
+  # Filter data to the relevant strategy index.
+  index <- index |> dplyr::filter(Strategy == strategy_name)
+
+  # Find start and end values for holding period.
+  start <- index |> dplyr::filter(Date == lubridate::as_date(start)) |> dplyr::pull(Value)
+  end   <- index |> dplyr::filter(Date == lubridate::as_date(end)) |> dplyr::pull(Value)
+
+  # Calculate return.
+  strat_return <- end/start - 1
+
+  return(strat_return)
+
+}
